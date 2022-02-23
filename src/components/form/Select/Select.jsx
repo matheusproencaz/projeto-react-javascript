5817fb94f7a1a57a9aa058b713e78e9d846e6ee7
Matheus Proenca <matheusproencaz@gmail.com>
2022-02-23 13:03:46 -0300

    Aula 5

--- src/components/form/Select/Select.jsx
+++ src/components/form/Select/Select.jsx
@@ -1,17 +1,23 @@
 import styles from "./Select.module.css";
 
-function Select({ text, name, options, handleOnChange, value }){
+function Select({ text, name, options, handleOnChange, value }) {
 
-    return( 
+    return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name} onChange={handleOnChange} value={value}>   
-                <option disabled>Selecione uma Opção</option>
+            <select
+                name={name}
+                id={name}
+            >
+                <option>Selecione uma opção</option>
+                {options.map((options) => (
+                    <option value={options.id} key={options.id}>
+                        {options.name}
+                    </option>
+                ))}
             </select>
-
         </div>
     );
-
 }
 
 export default Select;
--- src/components/projects/ProjectForm/ProjectForm.jsx
+++ src/components/projects/ProjectForm/ProjectForm.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Input from "../../form/Input/Input";
 import Select from "../../form/Select/Select";
 import SubmitButton from "../../form/SubmitButton/SubmitButton";
@@ -5,6 +6,22 @@ import styles from "./ProjectForm.module.css";
 
 function ProjectForm( { btnText }){
 
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setCategories(data)
+          })
+          //.catch(err => console.log(err))
+    }, [])
+   
     return(
         <form className={styles.form}>
             <div>
@@ -14,7 +31,7 @@ function ProjectForm( { btnText }){
                 <Input type="number" placeholder="Insira o orçamento total" name="budget" text="Orçamento do Projeto"/>
             </div>
             <div>
-                <Select name="category_id" text="Selecione a categoria"/>
+                <Select name="category_id" text="Selecione a categoria" options={categories}/>
             </div>
             <div>
                 <SubmitButton text={btnText} />
