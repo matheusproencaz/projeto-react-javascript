841df49f1d3fc15a6662b7657ba98aa038ec2c85
Matheus Proenca <matheusproencaz@gmail.com>
2022-02-23 10:16:47 -0300

    Aula 3

--- src/components/pages/NewProject/NewProject.jsx
+++ src/components/pages/NewProject/NewProject.jsx
@@ -1,10 +1,13 @@
+import styles from "./NewProject.module.css";
+import ProjectForm from "../../projects/ProjectForm/ProjectForm";
+
 function NewProject(){
 
     return(
-        <div>
+        <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>formulários</p>
+            <ProjectForm />
         </div>
         );
 }
